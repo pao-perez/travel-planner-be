3c0b19816f3099b6587ec8588bcc837f20aeec24
Pao Perez <pperez@hyperglance.com>
2024-09-04 18:37:24 -0600

    generate module, controller, service placeholders for city

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CityModule } from './city/city.module';
 
 @Module({
+  imports: [CityModule],
   controllers: [AppController],
   providers: [AppService],
 })
