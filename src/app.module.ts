a16270e68a49bd05f48d413f36c68a74d244fb6f
Pao Perez <pperez@hyperglance.com>
2024-09-04 23:44:25 -0600

    support cors and envs (local, dev)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CityModule } from './city/city.module';
 
 @Module({
-  imports: [CityModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.${process.env.NODE_ENV}`, '.env.local'],
+    }),
+    CityModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+
+  app.enableCors({
+    origin: configService.get('CLIENT_URI'),
+    methods: 'GET,POST,PUT,DELETE',
+  });
+
+  const port = configService.get<number>('SERVER_PORT');
+
+  await app.listen(port);
 }
 bootstrap();
