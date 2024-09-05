73b50f3c8fcd861538e2f9ce55e8baf771d16f44
Pao Perez <pperez@hyperglance.com>
2024-09-04 19:11:19 -0600

    feat: implement city controller routes /cities and /cities/:name

--- src/city/city.controller.ts
+++ src/city/city.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { CityService } from './city.service';
 
-@Controller('city')
-export class CityController {}
+@Controller('cities')
+export class CityController {
+  constructor(private readonly cityService: CityService) {}
+
+  @Get()
+  getAllCities() {
+    return this.cityService.findAll();
+  }
+
+  @Get(':name')
+  getCityByName(@Param('name') name: string) {
+    return this.cityService.findByName(name);
+  }
+}
