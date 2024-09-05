c9f46056e7b81f14e65d5a7ccbc48901c773d66d
Pao Perez <pperez@hyperglance.com>
2024-09-04 20:28:56 -0600

    chore: cleanup unused generated code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CityModule } from './city/city.module';
 
 @Module({
   imports: [CityModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
